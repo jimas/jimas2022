499fa11f052d88319a00a804a0a23b316ac9dd9c
jimas <lqj1110@sina.cn>
2023-05-23 14:12:26 +0800

    hbase demo

--- jimas-hadoop/jimas-hbase/src/main/java/com/jimas/hbase/HbaseDemo.java
+++ jimas-hadoop/jimas-hbase/src/main/java/com/jimas/hbase/HbaseDemo.java
@@ -1,5 +1,6 @@
 package com.jimas.hbase;
 
+import org.apache.commons.lang3.RandomUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellUtil;
@@ -12,6 +13,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author liuqj
@@ -19,7 +22,6 @@ import java.io.IOException;
 public class HbaseDemo {
     Connection connection = null;
     Admin admin = null;
-    Table table = null;
 
     @Before
     public void init() throws Exception {
@@ -27,14 +29,11 @@ public class HbaseDemo {
         configuration.set("hbase.zookeeper.quorum", "node03:2181,node04:2181,node05:2181");
         connection = ConnectionFactory.createConnection(configuration);
         admin = connection.getAdmin();
-        table = connection.getTable(TableName.valueOf("psn"));
-
-
     }
 
     @Test
     public void createTable() throws IOException {
-        TableName tableName = TableName.valueOf("psn2");
+        TableName tableName = TableName.valueOf("phone");
         TableDescriptor tableDescriptor = TableDescriptorBuilder.newBuilder(tableName)
                 .setColumnFamily(ColumnFamilyDescriptorBuilder.of(Bytes.toBytes("cf")))
                 .build();
@@ -47,23 +46,42 @@ public class HbaseDemo {
     }
 
     @Test
+    public void truncateTable() throws IOException {
+        TableName tableName = TableName.valueOf("psn");
+        admin.disableTable(tableName);
+        admin.truncateTable(tableName, true);
+    }
+
+    @Test
     public void getByRowKey() throws IOException {
+        Table table = connection.getTable(TableName.valueOf("psn"));
         Result result = table.get(new Get(Bytes.toBytes("11")));
         Cell cell = result.getColumnLatestCell(Bytes.toBytes("cf"), Bytes.toBytes("name"));
         System.out.println(Bytes.toString(CellUtil.cloneRow(cell)));
         System.out.println(Bytes.toString(CellUtil.cloneQualifier(cell)));
         System.out.println(Bytes.toString(CellUtil.cloneValue(cell)));
+        System.out.println(Bytes.toString(CellUtil.cloneFamily(cell)));
         System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("cf"), Bytes.toBytes("name"))));
+        table.close();
+    }
 
+
+
+    @Test
+    public void put() throws IOException {
+        String[] sexs = {"man", "woman"};
+        List<Put> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Put put = new Put(Bytes.toBytes(String.valueOf(i)));
+            put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("age"), Bytes.toBytes(String.valueOf(RandomUtils.nextInt(10, 50))));
+            put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("sex"), Bytes.toBytes(String.valueOf(sexs[RandomUtils.nextInt(0, 1)])));
+            put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("name"), Bytes.toBytes("lisi" + i));
+            list.add(put);
+        }
     }
 
     @After
     public void close() {
-        try {
-            table.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
         try {
             admin.close();
         } catch (IOException e) {
---
+++ jimas-hadoop/jimas-hbase/src/main/java/com/jimas/hbase/HbaseProtobuf.java
@@ -0,0 +1,138 @@
+package com.jimas.hbase;
+
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 通话记录表
+ * <br>rowkey:主叫+"_"+Long.Max-通话开始时间的时间戳</br>
+ * <br>被叫：dest:</br>
+ * <br>时长：duration:</br>
+ * <br>类型:type</br>
+ * <br>通话开始时间:callTime</br>
+ *
+ * @author liuqj
+ */
+public class HbaseProtobuf {
+    Connection connection = null;
+    Admin admin = null;
+    Table table = null;
+
+    @Before
+    public void init() throws Exception {
+        Configuration configuration = HBaseConfiguration.create();
+        configuration.set("hbase.zookeeper.quorum", "node03:2181,node04:2181,node05:2181");
+        connection = ConnectionFactory.createConnection(configuration);
+        admin = connection.getAdmin();
+        table = connection.getTable(TableName.valueOf("phone_buf"));
+    }
+
+    @Test
+    public void createTable() throws IOException {
+        TableName tableName = TableName.valueOf("phone_buf");
+        TableDescriptor tableDescriptor = TableDescriptorBuilder.newBuilder(tableName)
+                .setColumnFamily(ColumnFamilyDescriptorBuilder.of(Bytes.toBytes("cf")))
+                .build();
+
+        if (admin.tableExists(tableName)) {
+            admin.disableTable(tableName);
+            admin.deleteTable(tableName);
+        }
+        admin.createTable(tableDescriptor);
+    }
+
+    @Test
+    public void testScan() throws Exception {
+        Scan scan = new Scan();
+        //start：减大值，stop：减小值
+        scan.withStartRow(Bytes.toBytes("17699691383_" + (Long.MAX_VALUE - sdf.parse("20220228000000").getTime())));
+        scan.withStopRow(Bytes.toBytes("17699691383_" + (Long.MAX_VALUE - sdf.parse("20220212000000").getTime())));
+        ResultScanner scanner = table.getScanner(scan);
+        for (Result result : scanner) {
+            System.out.print(Bytes.toString(CellUtil.cloneRow(result.getColumnLatestCell(Bytes.toBytes("cf"), Bytes.toBytes("phone")))));
+            byte[] bytes = CellUtil.cloneValue(result.getColumnLatestCell(Bytes.toBytes("cf"), Bytes.toBytes("phone")));
+            Phone.Request request = Phone.Request.parseFrom(bytes);
+            System.out.print("--" + request.getDest());
+            System.out.print("--" + request.getType());
+            System.out.print("--" + request.getLength());
+            System.out.println(request.getCallTime());
+        }
+    }
+
+    @Test
+    public void insertDemoData() throws Exception {
+        List<Put> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            String tellA = getRandomNum("176");
+            for (int j = 0; j < 1000; j++) {
+                String dest = getRandomNum("158");
+                String length = String.valueOf(RandomUtils.nextInt(10, 100));
+                String type = String.valueOf(RandomUtils.nextInt(0, 1));
+                String callTime = getTime("2022");
+                Put put = new Put(Bytes.toBytes(tellA + "_" + (Long.MAX_VALUE - sdf.parse(callTime).getTime())));
+                Phone.Request build = Phone.Request.newBuilder()
+                        .setDest(dest)
+                        .setCallTime(callTime)
+                        .setLength(length)
+                        .setType(type).build();
+                put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("phone"), build.toByteArray());
+                list.add(put);
+            }
+        }
+        table.put(list);
+    }
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+
+    private String getTime(String time) {
+        return time + String.format("%02d", RandomUtils.nextInt(1, 12)) + String.format("%02d", RandomUtils.nextInt(1, 28))
+                + String.format("%02d", RandomUtils.nextInt(1, 24)) + String.format("%02d", RandomUtils.nextInt(1, 60))
+                + String.format("%02d", RandomUtils.nextInt(1, 60));
+    }
+
+    /**
+     * 不足时 用0 补齐
+     *
+     * @param prefix
+     * @return
+     */
+    private String getRandomNum(String prefix) {
+
+        return prefix + String.format("%08d", RandomUtils.nextInt(1, 99999999));
+    }
+
+    @After
+    public void close() {
+        try {
+            if (table != null) {
+                table.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            admin.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            connection.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
