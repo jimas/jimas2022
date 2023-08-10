bc15e4ee3c9da114d592d0a21cb75ab777aa10d8
jimas <lqj1110@sina.cn>
2023-08-10 16:09:45 +0800

    enueka

--- spring-eureka/eureka-consumer/src/main/java/com/jimas/eureka/consumer/controller/ConsumerOrderController.java
+++ spring-eureka/eureka-consumer/src/main/java/com/jimas/eureka/consumer/controller/ConsumerOrderController.java
@@ -1,5 +1,7 @@
 package com.jimas.eureka.consumer.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.jimas.common.entity.OrderDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,10 +21,10 @@ public class ConsumerOrderController {
     private RestTemplate restTemplate;
 
     @RequestMapping(value = "/consumer", method = RequestMethod.GET)
-    public String consumer(@RequestParam String orderNo) {
+    public OrderDTO consumer(@RequestParam String orderNo) {
         // 根据应用名称调用服务
         String json = restTemplate.getForObject("http://eureka-provider/order/findByNo?orderNo=" + orderNo, String.class);
         log.debug("remote result:{}", json);
-        return json;
+        return JSON.parseObject(json, OrderDTO.class);
     }
 }
