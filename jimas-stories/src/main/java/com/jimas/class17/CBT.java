a8d88fca76e544621b7b33f84fd4cca4f3833368
jimas <lqj1110@sina.cn>
2023-08-15 11:21:15 +0800

    递归

--- jimas-stories/src/main/java/com/jimas/class17/CBT.java
+++ jimas-stories/src/main/java/com/jimas/class17/CBT.java
@@ -8,19 +8,26 @@ import java.util.Queue;
  * ①：当节点左子树为空，右子树不为空 不是
  * ②：当中序遍历，遇到左右子树不全的节点时，且中序遍历的后续节点不是叶子节点  不是
  * 一棵深度为k的有n个结点的二叉树，对树中的结点按从上至下、从左到右的顺序进行编号，如果编号为i（1≤i≤n）的结点与满二叉树中编号为i的结点在二叉树中的位置相同，则这棵二叉树称为完全二叉树
+ *
  * @author liuqj
  */
 public class CBT {
 
     public static void main(String[] args) {
-        PrintBinaryTree.Node head = generateNodeTree(3);
-        System.out.println(isCbt(head));
-        PrintBinaryTree.printTree(head);
+        for (int i = 0; i < 100000; i++) {
+            PrintBinaryTree.Node head = generateNodeTree((int) (Math.random() * 10));
+            if (isCbt(head) != isCbt2(head)) {
+                System.out.println("error");
+                levelPrint(head);
+            }
+        }
+        System.out.println("success");
+
     }
 
     private static boolean isCbt(PrintBinaryTree.Node head) {
         if (head == null) {
-            return false;
+            return true;
         }
         boolean leaf = false;
         Queue<PrintBinaryTree.Node> queue = new LinkedList<>();
@@ -41,6 +48,54 @@ public class CBT {
             }
         }
         return true;
+    }
+
+    private static boolean isCbt2(PrintBinaryTree.Node head) {
+        if (head == null) {
+            return true;
+        }
+        return process(head).isCBT;
+    }
+
+    private static Info process(PrintBinaryTree.Node x) {
+        if (x == null) {
+            return new Info(true, true, 0);
+        }
+        Info leftInfo = process(x.left);
+        Info rightInfo = process(x.right);
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+        boolean isFull = leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height;
+        boolean isCBT = false;
+        if (leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height) {
+            isCBT = true;
+        } else if (leftInfo.isCBT && rightInfo.isFull && leftInfo.height == rightInfo.height + 1) {
+            isCBT = true;
+        } else if (leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height + 1) {
+            isCBT = true;
+        } else if (leftInfo.isFull && rightInfo.isCBT && leftInfo.height == rightInfo.height) {
+            isCBT = true;
+        }
+        return new Info(isCBT, isFull, height);
+    }
+
+    /**
+     * 递归套路：
+     * 如果x节点是完全二叉树，需要 其左子树以及右子树满足什么条件：
+     * 1、左子树是满树、且右子树是满树，并且 左高=右高
+     * 2、左子树是完全二叉树，且右子树是满树，并且左高=右高+1
+     * 3、左子树是满树，且右子树是满树，并且左高=右高+1
+     * 4、左子树是满树，且右子树是完全二叉树，并且左高=右高
+     */
+    private static class Info {
+        boolean isCBT;
+        boolean isFull;
+        int height;
+
+        public Info(boolean isCBT, boolean isFull, int height) {
+            this.isCBT = isCBT;
+            this.isFull = isFull;
+            this.height = height;
+        }
     }
 
     private static void levelPrint(PrintBinaryTree.Node head) {
